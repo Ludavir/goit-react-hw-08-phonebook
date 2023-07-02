d966137a41ca1d2f60f80ba05a27a1566d91d588
ludavir <david_arq.violin@yahoo.com>
2023-07-01 23:13:02 -0400

    yes?

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -3,7 +3,7 @@ import UserMenu from './usermenu/UserMenu';
 
 import { isUserLogin } from 'redux/auth/auth-selectors';
 import { useSelector } from 'react-redux';
-// import css from './NavBar.module.scss';
+import css from './NavBar.module.scss';
 
 const NavBar = () => {
   const isLogin = useSelector(isUserLogin);
